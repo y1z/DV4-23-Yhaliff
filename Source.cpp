304c33d87c1dd348a8fa176cfb7e8e86e87f3bc0
Yhaliff <idv17c.ybarraza@uartesdigitales.edu.mx>
2023-02-14 14:59:05 -0600

    Now it tells you how close you are

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cmath>
 
 void guess_number();
+bool guess_number_helper(int rand_num, int guessed_num);
 
 int main() {
 
@@ -29,31 +31,56 @@ int main() {
 
 void guess_number()
 {
-	int option = 0;
+	int guessed_num = 0;
 	int rand_number = rand() % 100;
 
 	bool keep_looping = true;
 	while (keep_looping) {
 		std::cout << "selecionar numero";
-		std::cin >> option;
+		std::cin >> guessed_num;
 
-		if (option > 99 || option < 0) {
+		if (guessed_num > 99 || guessed_num < 0) {
 			std::cout << "Fuera del rango permitido";
 		}
 		else {
-			if (option == rand_number) {
-				std::cout << "lo advinar el numero lograste siiiiii!I!II!Iii\n";
+			if (guess_number_helper(rand_number,guessed_num)) {
 				keep_looping = false;
 			}
-			if (option > rand_number) {
-				std::cout << "el numero es un numero menor\n";
-			}
-			if (option < rand_number) {
-				std::cout << "el number es un numero mayor\n ";
-			}
+			
 		}
 
 	}
 
 
+}
+
+// returns true when the guessed_num and rand_num are the same
+bool guess_number_helper(int rand_num, int guessed_num)
+{
+	if (guessed_num == rand_num) {
+		std::cout << "lo lograste advinar el numero siiiiii!I!II!Iii\n";
+		return true;
+	}
+	if (guessed_num > rand_num) {
+		std::cout << "el numero es un numero menor\n";
+	}
+	if (guessed_num < rand_num) {
+		std::cout << "el number es un numero mayor\n ";
+	}
+
+	// used to make sure no clue is repetead unnecesarly
+	if (std::abs(guessed_num - rand_num) <= 5) {
+		std::cout << "ya esta muy cerca\n";
+	}
+	else if (std::abs(guessed_num - rand_num) <= 15) {
+		std::cout << "te esta arcercando\n";
+	}
+	else if (std::abs(guessed_num - rand_num) <= 30) {
+		std::cout << "esta lejos\n";
+	}
+	else {
+		std::cout << "esta absudamente lejos\n";
+	}
+	
+	return false;
 }
